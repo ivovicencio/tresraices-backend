1c8bff35425129c67f85ed293b8a8ed8d11c2470
ivovicencio <frikimaniaco28@gmail.com>
2026-06-06 01:00:02 -0300

    feat: soporte DATABASE_URL para Railway + start script

--- db/db.js
+++ db/db.js
@@ -1,7 +1,14 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+const pool = new Pool(process.env.DATABASE_URL ? {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 5000,
+    maxUses: 7500
+} : {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
